cea3aa87438764720332492248a8b7e95c6442bb
SanjarKeySkills <forest2004@mail.ru>
2024-06-25 22:44:39 +0600

    Create button component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "./component/Button";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <React.Fragment>
+            <button content="Add" />
+            <button content="Download" />
+            <button content="Sign up" />
+            <button content="Delete" />
+            <button content="Close" />
+        </React.Fragment>
+    );
+};
 
 export default App;
